f199a1d0e78ef7ddfda9cf4632f6ee15fce384ef
Mikulas Plesak <mikulas.plesak@gmail.com>
2021-10-24 19:29:48 +0100

    functional sorting :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,7 @@ $.fn.scrollEnd = function(callback, timeout) {
 
 	// navigation window opening
 	$Bnav.on('click', function() {
+		// navigation is opening
 		if (nav_closed == true) {
 			$main.removeClass('main_WIDE');
 			$main.addClass('main_NARROW');
@@ -57,6 +58,7 @@ $.fn.scrollEnd = function(callback, timeout) {
 			$Bnav.addClass('fa-angle-double-left');
 			nav_closed = false;
 		} else {
+			// navigation is closing
 			$main.removeClass('main_NARROW');
 			$main.addClass('main_WIDE');
 			$nav_list.removeClass('nav_OPEN');
@@ -67,6 +69,199 @@ $.fn.scrollEnd = function(callback, timeout) {
 		}
 	});
 
+	// shuffling utility by communitywiki
+	// @ https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+	function shuffle(array) {
+		let currentIndex = array.length,  randomIndex;
+	  
+		// While there remain elements to shuffle...
+		while (currentIndex != 0) {
+	  
+		  // Pick a remaining element...
+		  randomIndex = Math.floor(Math.random() * currentIndex);
+		  currentIndex--;
+	  
+		  // And swap it with the current element.
+		  [array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+		}
+	  
+		return array;
+	}
+	// sorted utility by zer00ne
+	// @ https://stackoverflow.com/questions/53833139/check-array-in-js-is-list-sorted/53833620
+	function sorted(array) {
+		return array.every(function(num, idx, arr) {
+		  return (num <= arr[idx + 1]) || (idx === arr.length - 1) ? 1 : 0;
+		});
+	}
+	// get a random dark colour by Zaheer Ahmed
+	// @ https://stackoverflow.com/questions/20114469/javascript-generate-random-dark-color
+	function getDarkColor() {
+		var color = '#';
+		for (var i = 0; i < 6; i++) {
+			color += Math.floor(Math.random() * 10);
+		}
+		return color;
+	}
+
+	// 01: SORTING
+	var num_books = 5,
+		// plot axis elements
+		s01_x = [],
+		s01_y = [],
+		book_names = [],
+		picked = [],
+		running = false,
+		$library = $('.s01_wrap');
+
+	// clock timer adapted from the clock timer
+	// @ https://jsfiddle.net/wizajay/rro5pna3/305/
+	var s01_clock = {
+		totalSeconds: 0,
+		start: function () {
+			if (!this.interval) {
+				var self = this;
+				function pad(val) { return val > 9 ? val : "0" + val; }
+				this.interval = setInterval(function () {
+				self.totalSeconds += 1;
+		
+				if (self.totalSeconds >= 6000) {
+					$("#s01_min").text(pad(Math.floor(self.totalSeconds / 6000 % 60)))
+				}
+		
+				$("#s01_sec").text(pad(Math.floor(self.totalSeconds / 100 % 60)));
+				$("#s01_msec").text(pad(parseInt(self.totalSeconds % 100)));
+				}, 10);
+			}
+		},
+		
+		reset: function () {
+			s01_clock.totalSeconds = null; 
+			clearInterval(this.interval);
+			$("#s01_min").text("00");
+			$("#s01_sec").text("00");
+			$("#s01_msec").text("00");
+			delete this.interval;
+		},
+		pause: function () {
+			clearInterval(this.interval);
+			delete this.interval;
+		},
+		
+		resume: function () {
+			this.start();
+		},
+		
+		restart: function () {
+				this.reset();
+			s01_clock.start();
+		}
+	};
+
+	// method for picking a random book
+	book_names.random = function() {
+		var bk = this[Math.floor(Math.random()*this.length)];
+		while (picked.includes(bk) || bk.length > 30) {
+			bk = this[Math.floor(Math.random()*this.length)];
+		}
+		picked.push(bk);
+		return bk;
+	};
+
+	// call to set up the div of books to be sorted
+	function build_library(num_books) {
+		var book = 'x',
+			hor_space = $library.width()/num_books,
+			book_ids = Array.from(Array(num_books).keys()),
+			book_id = 0;
+		
+		console.log(book_ids);
+		shuffle(book_ids);
+			
+		for (let i = 0; i < num_books; i++) {
+			book_id = book_ids[i];
+			book = $('<li></li>')
+			.attr({
+				'class': 's01_book',
+				'data-bookid': book_id
+			})
+			.text(book_names.random())
+			.css({
+				'width': Math.min(60, hor_space),
+				'height': 150 + 100*(1+book_id)/num_books,
+				'background-color': getDarkColor()
+			});
+			$library.append(book);
+		}
+
+		picked = [];
+	};
+
+	// 100 best books by benoitvallon 
+	// @ https://github.com/benoitvallon/100-best-books/blob/master/books.json
+	$.getJSON('/assets/activities/books.json')
+		.done(function(data) {
+			$.each(data, function(key, value) {
+				book_names.push(value.title);
+			});
+			build_library(num_books);
+		});
+		// .fail() here as fallback
+
+	// two options for how to make the list sortable - both functional
+	// SortableJS @ https://github.com/SortableJS/Sortable
+	new Sortable(s01_wrap, {
+		animation: 300,
+		ghostClass: 'sortable-ghost',
+		onEnd: s01_check,
+		onStart: s01_start
+	});
+
+	// call to start timer
+	function s01_start() {
+		if (running == false) {
+			$('.s01_timer').css({'color': 'white'});
+			running == true;
+			s01_clock.start();
+		}
+	};
+
+	// call whenever user drops a book into the library
+	// checks if it is sorted and ends the game if so
+	function s01_check() {
+		var arr = []
+		$('.s01_book').each(function(i, item) {
+			arr.push($(item).data('bookid'));
+		})
+		console.log(arr);
+		if (sorted(arr)) {
+			$('.s01_timer').css({'color': '#00FA9A'});
+			running = false;
+			s01_clock.pause();
+			s01_x.push(num_books);
+			s01_y.push(s01_clock.totalSeconds);
+			console.log(s01_y);
+		}
+	};
+	// $library.sortable();
+
+	// library button functionality
+	function resetlibrary() {
+		$('.s01_timer').css({'color': 'white'});
+		s01_clock.reset();
+		$library.children().replaceWith();
+		build_library(num_books);
+	};
+	$('.s01_reset').on('click', resetlibrary);
+	$('.s01_harder').on('click', function() {
+		max_books = Math.floor($library.width()/70);
+		num_books = Math.min(num_books + 2, max_books);
+		num_books = Math.max(num_books, 3);
+		resetlibrary();
+	})
+
+	/* OVERLAY */
 	var $Boverlay = $('.Boverlay');
 
 	$Boverlay.on('click', function() {
