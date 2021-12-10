dc7c8314b1fe772e73bf0bed5fa13939376003fd
Mikulas Plesak <mikulas.plesak@gmail.com>
2021-12-10 16:06:56 +0000

    A3 basically works

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,6 @@ $.fn.scrollEnd = function(callback, timeout) {
 		}
 		return binomial_coeffs[n][k];
 	}
-	
 	// clock timer inspired by the clock timer
 	// @ https://jsfiddle.net/wizajay/rro5pna3/305/
 	class clock {
@@ -160,6 +159,20 @@ $.fn.scrollEnd = function(callback, timeout) {
 			this.reset();
 			this.start();
 		}
+	}
+	// shake function using shaking utility by elrumordelaluz - details in CSS
+	function shake(elem, time = 500, col = null) {
+		if (col) {
+			elem.css({'color': col});
+		}
+		elem.addClass('shake-hard shake-constant');
+		setTimeout(function() {
+			elem.removeClass('shake-hard shake-constant');
+			if (col) {
+				elem.css({'color': 'white'})
+			}
+		}, time);
+
 	}
 
 	// PLOTTING UTILITIES
@@ -349,6 +362,8 @@ $.fn.scrollEnd = function(callback, timeout) {
 			s01_ymax = Math.max(s01_ymax, time);
 			// redraw plot
 			update(s01_plt, s01_pts, s01_pts_full, s01_expl, s01_xmax, s01_ymax);
+		} else {
+			shake($('.s01_timer'), 200);
 		}
 	};
 
@@ -475,10 +490,7 @@ $.fn.scrollEnd = function(callback, timeout) {
 			update(s02_plt, pts, s02_pts_full, s02_expl);
 		} else {
 			// used to say / if (!corrbt && (ans == 'none' || ans == 'right'))
-			s02_clock.div.css({'color': 'red'});
-			setTimeout(() => {
-				s02_clock.div.css({'color': 'white'});
-			}, 100);
+			shake(s02_clock.div, time = 300, col = 'red');
 			ans = 'wrong';
 		}
 	}
@@ -520,7 +532,7 @@ $.fn.scrollEnd = function(callback, timeout) {
 		check(false)
 	});
 
-	/* on demand display next equation */
+	// on demand display next equation
 	$('.s02_next').on('click', function() {
 		round += 1;
 		s02_clock.restart();
@@ -610,7 +622,153 @@ $.fn.scrollEnd = function(callback, timeout) {
 	// ochcavka kdyz chci nic nezobrazovat protoze musi byt [s02_pts]
 	var s02_plt = $.plot('#s02_plot', s02_pts, s02_opt);
 
-	/* OVERLAYS */
+	// SECTION 03: arthur-merlin
+	var s03_coin = 'G1',
+		s03_permuted = false,
+		s03_restarted = true;
+
+	// graph general descriptions
+	var s03_style = [
+		{ selector: 'node',
+		  style: {'label': 'data(id)',
+				  'text-valign': 'center',
+				  'text-halign': 'center'}},
+		{ selector: 'edge',
+		  style: {'width': 3, 'line-color': '#333'}}
+		],
+		g1_elems = [
+		{ data: { id: 'A' },
+		  style: { 'background-color': 'cyan'} },
+		{ data: { id: 'B' },
+		  style: { 'background-color': 'beige'} },
+		{ data: { id: 'C' },
+		  style: { 'background-color': 'mediumpurple'} },
+		{ data: { id: 'D' },
+		  style: { 'background-color': 'pink'} },
+		{ data: { id: 'E' },
+		  style: { 'background-color': 'yellow'} },
+		{ data: { id: 'AB', source: 'A', target: 'B' } },
+		{ data: { id: 'AC', source: 'A', target: 'C' } },
+		{ data: { id: 'BC', source: 'B', target: 'C' } },
+		{ data: { id: 'CD', source: 'C', target: 'D' } },		
+		{ data: { id: 'CE', source: 'C', target: 'E' } },
+		{ data: { id: 'DE', source: 'D', target: 'E' } }
+		],
+		g2_elems = [
+			{ data: { id: '1' },
+			  style: { 'background-color': 'peru'} },
+			{ data: { id: '2' },
+			  style: { 'background-color': 'deeppink'} },
+			{ data: { id: '3' },
+			  style: { 'background-color': 'greenyellow'} },
+			{ data: { id: '4' },
+			  style: { 'background-color': 'lightskyblue'} },
+			{ data: { id: '5' },
+			  style: { 'background-color': 'orangered'} },
+			{ data: { id: '12', source: '1', target: '2' } },
+			{ data: { id: '13', source: '1', target: '3' } },
+			{ data: { id: '14', source: '1', target: '4' } },
+			{ data: { id: '15', source: '1', target: '5' } },
+			{ data: { id: '25', source: '2', target: '5' } },
+			{ data: { id: '34', source: '3', target: '4' } }		
+		];
+	// graphs
+	var g1 = cytoscape({
+		container: $('.s03_g1'),
+		elements: g1_elems,
+		style: s03_style,
+		layout: { name: 'concentric' }
+	});
+	var g2 = cytoscape({
+		container: $('.s03_g2'),
+		elements: g2_elems,
+		style: s03_style,
+		layout: { name: 'circle' }
+	});
+
+	// coin flip by Le Liu
+	// @ https://codepen.io/le0864/pen/pbmoVQ
+	function coinflip() {
+		var flipResult = Math.random();
+		$('#coin').removeClass();
+		setTimeout(function(){
+		  if(flipResult <= 0.5){
+			$('#coin').addClass('heads');
+			s03_coin = 'G1';
+		  }
+		  else{
+			$('#coin').addClass('tails');
+			s03_coin = 'G2';
+		  }
+		}, 100);
+	}
+
+	// functions guiding the activity
+	$('.s03_flip').on('click', coinflip);
+	$('.s03_permute').on('click', function() {
+		s03_permuted = true;
+		if (s03_restarted) {
+			// permute the right graph
+			if (s03_coin == 'G2') {
+				console.log('permuting G2');
+				$('.s03_gq').css({'background-color':'gold'});
+				var gq = cytoscape({
+					container: $('.s03_gq'),
+					elements: g2_elems,
+					style: s03_style,
+					layout: {name: 'cose'}
+				});
+			} else {
+				console.log('permuting G1', g1.elements());
+				$('.s03_gq').css({'background-color':'silver'});
+				var gq = cytoscape({
+					container: $('.s03_gq'),
+					elements: g1_elems,
+					style: s03_style,
+					layout: {name: 'cose'}
+				});
+			}
+		} else {
+			shake($('.s03_restart'), 300);
+		}
+	});
+	$('.s03_send').on('click', function() {
+		console.log(s03_permuted && s03_restarted);
+		if (s03_permuted && s03_restarted) {
+			$('.s03_arthurtext').css({'opacity':'1'});
+			let merlinsays = Math.random() > 0.5 ? 'G1' : 'G2';
+			$('.s03_merlintext').text('My magic tells me that it is from ' + merlinsays);
+			var gq_left = $('.s03_gq').offset().left,
+				ga_left = $('.s03_ga').offset().left;
+			setTimeout(function() {
+				let dist = ga_left-gq_left + 'px';
+				$('.s03_gq').css({left: dist});
+				$('.fa-magic').addClass('loading');
+				setTimeout(function() {
+					$('.fa-magic').removeClass('loading');
+					$('.s03_merlintext').css({'opacity':'1'});
+				}, 3000)
+			}, 500);
+			s03_restarted = false;
+		} else if (s03_permuted) {
+			// let the user know they need to restart
+			shake($('.s03_restart'), 300);
+		} else {
+			// let the user know they need to permute
+			shake($('.s03_permute'), 300);
+		}
+	});
+	$('.s03_restart').on('click', function() {
+		$('.s03_gq').css({'background-color': 'rgba(0, 0, 0, 0)', 'transition': '0s'});
+		$('.s03_gq').empty();
+		$('.s03_gq').css({left: 0});
+		$('.s03_text').css({opacity: 0});
+		s03_restarted = true;
+		s03_permuted = false;
+		setTimeout(function() {$('.s03_gq').css({'transition': '1s'});}, 1000);
+	});
+
+	// OVERLAYS
 	var bexplain = false,
 		blinks = false,
 		s01_expl = false,
@@ -662,5 +820,6 @@ $.fn.scrollEnd = function(callback, timeout) {
 			blinks = false;
 		}
 	}
+
 
 })(jQuery);
